ad2bfedbc8e389a3b6caa06464dc39d16f4bc601
ftrace <eunchul.jung@nhn-japan.com>
2021-09-16 17:50:57 +0900

    update: view - for ec connection

--- helloweb/info/views.py
+++ helloweb/info/views.py
@@ -1,3 +1,4 @@
+import socket
 import subprocess
 from django.shortcuts import render
 
@@ -33,6 +34,19 @@ def index(request):
         'cmd': ' '.join(cmd),
         'stdout': stdout.decode(),
         'stderr': stderr.decode(),
+    })
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    res = sock.connect_ex(('ec.allu.dev', 6379))
+    _stdout = 'not open'
+    if res == 0:
+        _stdout = 'open'
+    sock.close()
+
+    results.append({
+        'cmd': 'telnet ec.allu.dev 6379',
+        'stdout': _stdout,
+        'stderr': '',
     })
 
     return render(request, 'info/index.html', {
